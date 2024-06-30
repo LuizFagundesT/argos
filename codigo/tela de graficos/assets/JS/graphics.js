be8bd240e1366f6654a22a2fb06fb5dfcc2d3eca
Luiz Gustavo Fagundes Teixeira <luizagustinho032@gmail.com>
2024-06-30 20:55:01 -0300

    atualização final

--- codigo/tela de graficos/assets/JS/graphics.js
+++ codigo/tela de graficos/assets/JS/graphics.js
@@ -1,20 +1,56 @@
-const ctx = document.getElementById('chart').getContext('2d');
+function criarGrafico(botao) {
+  var ctx = document.getElementById('chart').getContext('2d');
 
-let chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'],
-      datasets: [{
-        label: 'Todos os Gráficos',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 5,
-        borderColor: 'rgba(77,166,253,0.85)',
-        backgroundColor: 'blue',
-      }]
-    },
-    options: {
-        Animation: {
-            duration:5000
-        }
-    }
+  // Dados e cores para cada botão
+  var dados, cores;
+
+  // Definição dos dados e cores baseado no botão clicado
+  switch (botao) {
+      case 'button1':
+          dados = [12, 19, 3, 5, 2];
+          cores = ['rgba(165,42,42)'];
+          backgroundColor=['rgba(255,0,0)']
+          break;
+      case 'button2':
+          dados = [10, 15, 7, 8, 5];
+          cores = ['rgba(50,205,50)'];
+          backgroundColor=['rgba(0, 255, 0)'];
+          break;
+      case 'button3':
+          dados = [8, 12, 10, 6, 4];
+          cores = ['rgba(77,166,253,0.85)'];
+          backgroundColor=['rgba(0,0,255)'];
+          break;
+      default:
+          dados = [];
+          cores = [];
+          backgroundColor= []  
+  }
+
+  // Limpa o canvas antes de criar um novo gráfico
+  if (window.grafico) {
+      window.grafico.destroy();
+  }
+
+  // Cria um novo gráfico de barras com base nos dados e cores definidos
+  window.grafico = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'],
+          datasets: [{
+              label: 'Denúncias',
+              data: dados,
+              backgroundColor: cores,
+              borderColor: backgroundColor, // Ajusta a opacidade para o contorno
+              borderWidth: 5
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
   });
+}
